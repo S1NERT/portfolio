71b7728674bead49888df672b6db18be74b69c63
S1NERT <52111956+S1NERT@users.noreply.github.com>
2023-12-15 14:50:32 -0500

    init commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,11 @@
 <script>
+	import '../app.pcss';
 	import Header from './Header.svelte';
 	import './styles.css';
 </script>
 
 <div class="app">
-	<Header />
+	<Header></Header>
 
 	<main>
 		<slot />
@@ -22,32 +23,9 @@
 		min-height: 100vh;
 	}
 
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
+	main,
 	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
+		width: min(100% - 3rem, 960px);
+		margin-inline: auto;
 	}
 </style>
