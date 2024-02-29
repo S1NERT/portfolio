76e817a0f6f62e8479b91b428f5a8e320f012853
S1NERT <52111956+S1NERT@users.noreply.github.com>
2024-02-29 11:01:37 -0500

    Maintenence

--- src/stores/projects.ts
+++ src/stores/projects.ts
@@ -22,7 +22,7 @@ const projects = writable([
         src: Pridefultrinkets,
         alt: '',
         quickDesc: 'A multi-sensory exhibit created for the Queer Youth Exhibition',
-        longDesc: ' co-hosted by The Design Museum Everywhere. The exhibit utilizes small take home objects that a user approaches the table, it lights up and plays a small audio sample through a custom designed',
+        longDesc: ' co hosted by The Design Museum Everywhere. The exhibit utilizes small take home objects that are collected at tables placed around the space and when put together form a unicorn. The tables are placed intentionally to guide people towards other events in the space. In addition when a user approaches the table, it lights up and plays a small audio sample through a custom designed audio driver, corresponding to the closest event along with showcasing a prominent LGBTQ+ figure.',
         projectTitle: '-Prideful Trinkets',
     },
 
@@ -49,8 +49,8 @@ const projects = writable([
     {
         src: SenecaVil,
         alt: '',
-        quickDesc: 'The essay The Dynamics of Eminent Domain: Revisiting Urban Narratives, focus one eminent domains evolution',
-        longDesc: ' through a series of court cases that slowly expanded the scope of the law as well as highlighting controversial implementations of eminent domain such as the story of Seneca Village. The essay concludes with suggestions to improve and redefine eminent domain powers.',
+        quickDesc: 'The research paper The Dynamics of Eminent Domain: Revisiting Urban Narratives, focus one eminent domains evolution',
+        longDesc: ' through a series of court cases that slowly expanded the scope of the law as well as highlighting controversial implementations of eminent domain such as the story of Seneca Village. The paper concludes with a series of actionable solutions to improve and redefine eminent domain powers.',
         projectTitle: '-The Dynamics of Eminent Domain',
 
 
