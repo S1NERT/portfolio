e20ec70570a6c5d15e1a81689cb8c379f6734354
S1NERT <52111956+S1NERT@users.noreply.github.com>
2024-02-29 10:27:29 -0500

    Added Thumb

--- src/stores/projects.ts
+++ src/stores/projects.ts
@@ -4,6 +4,7 @@ import Pridefultrinkets from '$lib/images/Pridefultrinkets.png';
 import InclusiveMicro from '$lib/images/InclusiveMicro.png';
 import SenecaVil from '$lib/images/SenecaVillage.png'
 import Misinfo from '$lib/images/Misinfo.png'
+import Thumb from '$lib/images/Thumb.png'
 
 const projects = writable([
     {
@@ -37,8 +38,18 @@ const projects = writable([
     {
         src: Misinfo,
         alt: '',
-        quickDesc: 'Misinformed Mayhem is a collaborative board game that aims to educate players about the spread of misinformation.',
-        longDesc: ' The game is designed to represent the exponential spread of misinformation, which eventually reaches a plateau due to a lack of viable targets.By playing Misinformed Mayhem, players can gain a better understanding of the patterns and spread of misinformation.The game is specifically targeted towards teenagers, who are often the most susceptible to misinformation due to their high levels of online activity and lack of experience with discerning fact from fiction.',
+        quickDesc: 'Misinformation Mayhem is a collaborative board game that aims to educate players about the spread of misinformation.',
+        longDesc: ' The game is designed to represent the exponential spread of misinformation, which eventually reaches a plateau due to a lack of viable targets.By playing Misinformation Mayhem, players can gain a better understanding of the patterns and spread of misinformation.The game is specifically targeted towards teenagers, who are often the most susceptible to misinformation due to their high levels of online activity and lack of experience with discerning fact from fiction.',
+        projectTitle: '-Misinformation Mayhem',
+
+
+    },
+
+    {
+        src: Thumb,
+        alt: '',
+        quickDesc: 'As the field of prosthetics evolves, it not only offers solutions for replacing lost limbs but also opens new avenues for augmenting human capabilities beyond their natural boundaries.',
+        longDesc: ' The Third Thumb project epitomizes this shift, representing a convergence of robotics, and design to enhance the human bodys functionality. Third Thumb aims to redefine human potential by enabling users to perform complex tasks with unprecedented ease and efficiency, thus challenging traditional perceptions of disability and augmentation. By focusing on the integration of this technology with the human sensory- motor system, the Third Thumb project not only demonstrates the tangible benefits of advanced prosthetics in improving everyday life but also explores the future possibilities of human - machine symbiosis.',
         projectTitle: '-The Dynamics of Eminent Domain',
 
 
